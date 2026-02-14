54125d744cfcc3def1e6dffae7748932320f549e
Paul Victor Raj <paulvictor@gmail.com>
2026-02-14 22:55:30 +0530

    From previous work adding a keyboard

---
+++ keyboards/ferris/keymaps/paulvictor/rules.mk
@@ -0,0 +1,10 @@
+MOUSEKEY_ENABLE = yes
+COMBO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+EXTRAFLAGS += -flto
+UNICODE_ENABLE   = no # Unicode
+SWAP_HANDS_ENABLE= no  # Allow swapping hands of keyboard
